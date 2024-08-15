aaccd725835dcd22595a0a3c0beef02a0a1d1c43
JeskoCGN <dj_jesko@icloud.com>
2024-08-15 18:50:44 +0200

    Adjusted Player Controller, Player Assets not implemented yet - gonna do tomorrow

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,21 +12,36 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private float gravityForce;
     private Vector2 moveDirectionGround;
+    [SerializeField] private float minVelocity;
+    [SerializeField] private float maxVelocity;
+
+
+    [SerializeField] public float globalGravity = -9.81f;
+    [SerializeField] public float gravityScale = 1.0f;
 
     private void Awake()
     {
-        
+
     }
 
     private void Update()
     {
-        
+        Debug.Log(rb3D.velocity);
     }
 
     private void FixedUpdate()
     {
-        rb3D.AddForce(-moveDirectionGround.x * moveSpeed, 0f, -moveDirectionGround.y * moveSpeed, ForceMode.Acceleration);
-        
+        rb3D.AddForce(moveDirectionGround.x * moveSpeed, 0f, moveDirectionGround.y * moveSpeed, ForceMode.Acceleration);
+
+        if (new Vector3(rb3D.velocity.x, 0, rb3D.velocity.z).magnitude > maxVelocity)
+        {
+            Vector3 newSpeed = Vector3.Normalize(rb3D.velocity) * maxVelocity;
+            rb3D.velocity = new Vector3(newSpeed.x, rb3D.velocity.y, newSpeed.z);
+        }
+
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        rb3D.AddForce(gravity, ForceMode.Acceleration);
+
     }
 
     public void MovePlayer(InputAction.CallbackContext context)
@@ -47,9 +62,6 @@ public class PlayerController : MonoBehaviour
         if (context.performed)
         {
             rb3D.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.VelocityChange);
-            
         }
     }
-
-
 }
